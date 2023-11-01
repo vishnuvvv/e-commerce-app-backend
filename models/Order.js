1052fc1de9e5579b6d7aa1e206d2f176cd2470a3
vishnu <vishnusathyanathan@gmail.com>
2023-11-01 10:25:37 +0530

    address

--- models/Order.js
+++ models/Order.js
@@ -15,7 +15,7 @@ const orderSchema = new mongoose.Schema(
       },
     ],
     amount: { type: Number, required: true },
-    address: { type: Object, required: true },
+    address: { type: Object},
     status: { type: String, default: "pending" },
   },
   { timestamps: true }
