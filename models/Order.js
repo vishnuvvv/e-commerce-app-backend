d1e1fd64857c06b5e63639305c3f0daf4d38f075
vishnu <vishnusathyanathan@gmail.com>
2023-11-08 00:57:30 +0530

    modified order schema

--- models/Order.js
+++ models/Order.js
@@ -5,17 +5,35 @@ const orderSchema = new mongoose.Schema(
     userId: { type: String, requied: true },
     products: [
       {
-        productId: {
+        _id: {
           type: String,
         },
         quantity: {
           type: Number,
           default: 1,
         },
+        size: [{ type: String }],
+        instok: {
+          type: String,
+        },
+        title: {
+          type: String,
+        },
+        img: {
+          type: String,
+        },
+        color: {
+          type: String,
+        },
+        desc: {
+          type: String,
+        },
+        categories: [{ type: String }],
+        price: { type: Number },
       },
     ],
     amount: { type: Number, required: true },
-    address: { type: Object},
+    address: { type: String },
     status: { type: String, default: "pending" },
   },
   { timestamps: true }
